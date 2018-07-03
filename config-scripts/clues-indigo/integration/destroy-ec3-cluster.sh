93be37fcdc92c7feea077f3f1d96029c24fec5f4
Ignacio Blanquer <iblanque@veraw57-147.wi-fi.upv.es>
2018-07-03 12:21:08 +0200

    ATMOSPHERE repository preparation

--- config-scripts/clues-indigo/integration/destroy-ec3-cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash +x
-NAME=$1
-# Set working directory
-cd /opt/ec3/
-# Destroy the cluster
-./ec3 destroy $NAME -y
--- config-scripts/clues-indigo/integration/launch-ec3-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash +x
-NAME=$1
-AUTH_FILE=$2
-# Set working directory
-cd /opt/ec3/
-# Launch cluster
-./ec3 launch $NAME mesos docker ubuntu14-ramses -a $AUTH_FILE -u http://servproject.i3m.upv.es:8899 -y
