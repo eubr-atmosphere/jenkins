98d7632a2256bf255819b9b3a31063f288ef1c7b
Ignacio Blanquer <iblanque@MacBook-Pro-de-Ignacio.local>
2018-09-12 09:38:32 +0200

    base-notebook start modified for ipycluster and ssh

--- docker/ubuntu/16-04/notebook/base-notebook/start-notebook.sh
+++ docker/ubuntu/16-04/notebook/base-notebook/start-notebook.sh
@@ -4,6 +4,37 @@
 
 set -e
 
+if [ "$SKIPMOUNT" != "true" ]; then
+  mkdir ~/.ssh
+  echo "SSH Directory created to"
+  echo $SSHPRIVKEY | sed -e 's/\\n/\n/g' > ~/.ssh/id_rsa
+  chmod 400 ~/.ssh/id_rsa
+  echo $SSHPUBKEY > ~/.ssh/id_rsa.pub
+  
+  echo "PRIVKEY:"
+  ls -l ~/.ssh/id_rsa
+  cat ~/.ssh/id_rsa
+  echo "PUBKEY:"
+  ls -l ~/.ssh/id_rsa.pub
+  cat ~/.ssh/id_rsa.pub
+
+  echo "Remove host verification"
+
+  echo "Host $SERVER" > ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  chmod 400 ~/.ssh/config
+
+  echo "Connecting to root@$SERVER"
+  HOMEUSER=$HOME
+  sudo sshfs -o allow_other -o IdentityFile=$HOMEUSER/.ssh/id_rsa -o StrictHostKeyChecking=no root@$SERVER:/ $MOUNTPOINT
+fi
+
+
+if [ "$STARTCLUSTER" == "true" ]; then
+  MYIP=$(hostname -i)
+  ipcontroller --ip=$MYIP --ipython-dir=$MOUNTPOINT/.ipython &
+fi
+
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
   # launched by JupyterHub, use single-user entrypoint
   exec /usr/local/bin/start-singleuser.sh $*
